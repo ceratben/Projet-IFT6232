7d7b9ea00478dedf20ddca6982df87ba1ddc9529
Benjamin Cerat <zbingu@gmail.com>
2012-02-21 16:15:42 -0500

    Avec macro

--- miniscm.scm
+++ miniscm.scm
@@ -2,43 +2,69 @@
 
 ;;; File: "miniscm.scm"
 
-(define-macro (match sujet . clauses) 
-  (define (conditions exp gab) (cond ((null? gab) 
-				      (list `(null? ,exp))) 
-				     ((symbol? gab) 
-				      (list `(eq? ,exp ’,gab))) 
-				     ((number? gab) 
-				      (list `(eqv? ,exp ,gab))) 
-				     ((pair? gab) 
-				      (append (list `(pair? ,exp)) 
-					      (conditions `(car ,exp) (car gab)) 
-					      (conditions `(cdr ,exp) (cdr gab)))) 
-				     (else 
-				      (error "unknown pattern")))) 
-  (define (if-equal? var gab oui non) 
-    (cond ((and (pair? gab) 
-		(eq? (car gab) `unquote) 
-		(pair? (cdr gab)) 
-		(null? (cddr gab))) 
-	   `(let ((,(cadr gab) ,var)) 
-	      ,oui)) 
-	  ((null? gab) 
-	   `(if (null? ,var) ,oui ,non)))) 
-    (define (gen var clauses) 
-    (if (pair? clauses) 
-	(let ((clause (car clauses))) 
-	  (if-equal? var 
-		     (car clause) 
-		     (cadr clause) 
-		     (gen var (cdr clauses)))) 
-	`(error "match failed"))) 
-  (let ((var (gensym))) 
-    `(let ((,var ,sujet)) 
-       ,(gen var clauses))))
 
+;;; Match
+
+(define-macro (match sujet . clauses)
+  (define (if-equal? var gab oui non)
+    (cond ((and (pair? gab)
+		(eq? (car gab) 'unquote)
+		(pair? (cdr gab))
+		(null? (cddr gab)))
+	   `(let ((,(cadr gab) ,var))
+	      ,oui))
+	  ((null? gab)
+	   `(if (null? ,var) ,oui ,non))
+	  ((symbol? gab)
+	   `(if (eq? ,var ',gab) ,oui ,non))
+	  ((or (boolean? gab)
+	       (char? gab))
+	   `(if (eq? ,var ,gab) ,oui ,non))
+	  ((number? gab)
+	   `(if (eqv? ,var ,gab) ,oui ,non))
+	  ((pair? gab)
+	   (let ((carvar (gensym))
+		 (cdrvar (gensym)))
+	     `(if (pair? ,var)
+		  (let ((,carvar (car ,var)))
+		    ,(if-equal? carvar (car gab)
+				`(let ((,cdrvar (cdr ,var)))
+				   ,(if-equal? cdrvar (cdr gab) oui non))
+				non))
+		  ,non)))
+	  (else
+	   (error "unknown pattern"))))
+  
+  (let* ((var (gensym))
+	 (fns (map (lambda (x) (gensym)) clauses))
+	 (err (gensym)))
+    `(let ((,var ,sujet))
+       ,@(map (lambda (fn1 fn2 clause)
+		`(define (,fn1)
+		   ,(if-equal? var
+			       (car clause)
+			       (if (and (eq? (cadr clause) 'when)
+					(pair? (cddr clause)))
+				   `(if ,(caddr clause) ,(cadddr clause) (,fn2))
+                                     (cadr clause))
+                                 `(,fn2))))
+                fns
+                (append (cdr fns) (list err))
+                clauses)
+         (define (,err) (error "match failed"))
+         (,(car fns)))))
+
+(define gensym ;; une version de gensym utile pour le deboguage
+  (let ((count 0))
+      (lambda ()
+        (set! count (+ count 1))
+        (string->symbol (string-append "g" (number->string count))))))
+
+;;;;;
 
 (define gcte '(argc))
 (define grte '(1))
+(define macro-list '())
 
 (define env-lookup 
   (lambda (data env pos)
@@ -47,6 +73,13 @@
 	  (if (null? (cdr env)) #f
 	      (env-lookup data (cdr env) (+ pos 1)))
 	  )))
+
+;;; de stack overflow
+(define (list-set! l k obj)
+  (cond
+    ((or (< k 0) (null? l)) #f)
+    ((= k 0) (set-car! l obj))
+    (else (list-set! (cdr l) (- k 1) obj))))
 
 ;;; Executing define:
 
@@ -61,8 +94,45 @@
 	      (set! gcte (append (cons (list-ref expr 1) '()) gcte)) 
 	      (set! grte (append (cons (comp-expr (list-ref expr 2) 0 gcte grte) '())grte))
 	      (print grte)))
+	   ((and (list? expr)
+              (= (length expr) 3)
+              (eq? (list-ref expr 0) 'define-macro)) 
+	    (let ((sig (list-ref expr 1)) (body (list-ref expr 2)))
+	      (set! macro-list (cons (cons sig body) macro-list))))
 	   )))
 
+(define macro-lookup
+  (lambda (x l)
+      (and (pair? l) 
+	   (if (eq? x (caaar l))
+	       (car macro-list)
+	       (macro-lookup x (cdr l)))
+	   )))
+
+(define expand-macro
+  (lambda (expr macr)
+      (let* ((fpara (map list (cdr expr) (cdar macr))) (body (cdr macr)) 
+	    (f (lambda (a) 
+		 (let ((x (assoc a fpara)))
+		   (if x (cdr x) a)))))
+	(if (list? body)
+	      (map f body)
+	      (f body))
+	  )))
+
+(define apply-macro 
+  (lambda (ast)
+      (if (pair? ast) 
+	   (let ((x (macro-lookup (car ast) macro-list)) (params (map apply-macro (cdr ast))))
+	     (if x
+		 (let ((newTok (eval (expand-macro (cons (car ast) params) x))))
+		   (begin 
+		     (defining newTok)
+		     (apply-macro newTok)
+		     ))
+		 (cons (car ast) params))
+	     )
+	   ast)))
 
 ;;; Translation of AST to machine code
 
@@ -74,15 +144,16 @@
                 (comp-expr expr 0 gcte grte)))
 
 (define (comp-expr expr fs cte rte) ;; fs = frame size
-                                ;; cte = compile time environment
+                                    ;; cte = compile time environment
 
   (cond ((number? expr)
          (gen-literal expr))
 
-	((string? expr)
-	 (begin (pp expr) '()))
+	;;;a faire
+	;;;((string? expr)
+	 ;;;(begin (pp expr) ""))
 
-	;;; A changer pour liste separee.
+	;;; Var
         ((symbol? expr)
          (let ((x (env-lookup expr cte 0)))
            (if x
@@ -93,15 +164,21 @@
 		 )
 	       ;;; retourne un litéral?
                (error "undefined variable" expr))))	
-	
-	;;; begin: Utilise env+cte au lieu de cte pour inclure les definitions precedentes dans la seq.
+
+	;;; if
+	((and (list? expr) (= (length expr) 4) (eq? (car expr) 'begin))
+	 (let ((c-expr (map (lambda (x) (comp-expr x fs cte rte)) (cdr expr)))) 
+	   (gen-if (cadr c-expr) (caddr c-expr) (cadddr expr))))
+
+	;;; begin à bouclifier.
 	((and (list? expr) (eq? (car expr) 'begin))
-	 (if (= (length expr) 2)
-	     (comp-expr (cadr expr) fs cte rte)
-	     (cons 
-	       (comp-expr (cadr expr) fs cte rte)
-	       (comp-expr (cons 'begin (cddr expr)) fs cte rte))
-	  ))
+	 (let ((ret '())) (begin
+	   (let loop ((n 1))
+	       (and (< n (length expr))
+		   (begin
+		     (set! ret (append (cons (comp-expr(list-ref expr n) fs cte rte) '()) ret))
+		     (loop (+ n 1)))
+		   ))) ret))
 
 	;;; let to change
         ((and (list? expr)
@@ -126,12 +203,23 @@
          
 	 ""
 	)
+	((and (list? expr)
+              (= (length expr) 3)
+              (eq? (list-ref expr 0) 'define-macro))
+         
+	 ""
+	)
 
-	;;; set!
+	;;; set! doesn't work!
+	;;; define devrait associer un espace memoire avec malloc.
 	((and (list? expr) (= (length expr) 3) (eq? (car expr) 'set!))
-	 '())
+	 (let ((i (env-lookup (list-ref expr 1) cte 0)))
+	   (if i 
+	       (list-set! rte i (comp-expr (list-ref expr 2) fs cte rte)) 
+	       (error "Unbound variable" (list-ref expr 1)))
+	  ))
 
-	;;; lambda to patch env
+	;;; lambda
         ((and (list? expr)
               (= (length expr) 3)
               (eq? (list-ref expr 0) 'lambda))
@@ -179,6 +267,8 @@
        code
        "    ret\n"))
 
+(define (gen-if c t f) "")
+
 (define (gen-lambda args body) "")           ;;; to implement
 (define (gen-call fun args) "")              ;;; to implement
 
@@ -215,7 +305,8 @@
   (let ((ast (parse source-filename)))
     (let ((code (begin 
 		  (defining ast)
-		  (translate ast))))
+		  (translate 
+		   (apply-macro ast)))))
       (with-output-to-file
           (string-append (path-strip-extension source-filename) ".s")
         (lambda ()
